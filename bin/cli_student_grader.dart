2eecc24becd9a5f2e9e2da832e3754528477af78
Arick077 <188138560+Arick077@users.noreply.github.com>
2026-04-18 22:14:39 +0600

    feat: add menu loop and switch routing

--- bin/cli_student_grader.dart
+++ bin/cli_student_grader.dart
@@ -1,3 +1,68 @@
+import 'dart:io';
 void main () {
-    print("hello world");
+    const appTitle = "Student Grader v1.0";
+    final subjects = {"Math","English","Science","ICT"};
+     var students = <Map<String, dynamic>> [
+        {
+            "name":     "Fatima",
+            "scores":   [85, 92, 78],        // List<int> — one per subject
+            "subjects": {"Math", "English", "Science"},  // Set<String>
+            "bonus":    null,              // int? — optional bonus points
+            "comment":  null   
+        }
+        
+    ];
+    int choices ;
+    do{
+        print ('''
+        ===== $appTitle =====
+
+                1. Add Student
+                2. Record Score
+                3. Add Bonus Points
+                4. Add Comment
+                5. View All Students
+                6. View Report Card
+                7. Class Summary
+                8. Exit
+
+            ''');
+        print("Choose an option: ");
+
+        choices = int.tryParse(stdin.readLineSync() ?? '0') ?? 0;
+        
+        
+        switch(choices)
+        {
+            case 1:
+                print("Add Student");
+                break;
+            case 2:
+                print("Record Score");
+                break;
+            case 3:
+                print("Add Bonus Points");
+                break;
+            case 4:
+                print("Add Comment");
+                break;
+            case 5:
+                print("View All Students");
+                break;
+            case 6:
+                print("View Report Card");
+                break;
+            case 7:
+                print("Class Summary");
+                break;
+            case 8:
+                print("Exit");
+                break;
+            default:
+            print("Invalid choice");
+        }
+    }while(choices != 8);
+
+
+
 }
